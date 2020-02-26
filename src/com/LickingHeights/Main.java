206dddc2c502a97c914f29f5eeba584ef630d05c
Garrett Miller <garrett.miller.2020@student.lhschools.org>
2020-02-26 13:18:11 -0500

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,30 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ModulusClock();
+    }
+
+        public static void ModulusClock(){
+            Scanner Keyboard;
+            Keyboard = new Scanner(System.in);
+            System.out.println("Enter a value for hours");
+            int number = Keyboard.nextInt();
+
+            Scanner keyboard;
+            keyboard = new Scanner(System.in);
+            System.out.println("Enter a value for minutes");
+            int Number = Keyboard.nextInt();
+
+
+
+
+
+
+
     }
 }
